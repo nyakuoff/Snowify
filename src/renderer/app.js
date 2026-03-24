126dc0837eea46b07354118f7bf4c3e668c18d03
alexeysuper <alexeysuper0@gmail.com>
2026-03-24 16:42:34 +0200

    feat(i18n): add translations to country setting hint (Russian)

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -3395,7 +3395,7 @@ const cachedPath = prefetchCache.getCachedPath(track.id);
     if (!state.country) {
       html += `<div class="explore-country-hint">
         <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>
-        <span>Set your <a href="#" id="explore-country-link">country in Settings</a> for more relevant recommendations</span>
+        <span>${I18n.t('explore.setYour')} <a href="#" id="explore-country-link">${I18n.t('explore.countrySettings')}</a> ${I18n.t('explore.forRecommendations')}</span>
       </div>`;
     }
 
